2dbff8e30683c1bec13eb66b6301b430b50f7c26
tobikoleriari <peter.leriari@strathmore.edu>
2024-09-04 15:15:56 +0300

    oop

---
+++ Menus/menus.php
@@ -0,0 +1,11 @@
+<?php
+class menus{
+    public function main_menu(){
+        ?>
+        <div>
+            <a href="./">Home</a>
+            <a href="./">Home</a>
+    </div>
+        <?php
+    }
+}
--- load.php
+++ load.php
@@ -2,4 +2,20 @@
 require_once "fnc.php";
 
 $Obj = new fnc();
+
+$arr =["black","white","red","blue"];
+foreach($arr as $colour){
+    print $colour . "<br>";
+}
+print dirname(__FILE__);
+print "<br>";
+print $_SERVER["PHP_SELF"];
+print "<br>";
+print basename($_SERVER["PHP_SELF"]);
+print "<br>";
+if(file_exists("index.php") AND is_readable("index.php")){
+    print "yesssss";
+}else{
+    print "nooooo";
+}
 ?>
