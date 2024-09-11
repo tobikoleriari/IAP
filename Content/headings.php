a57fb36cbbe67e40fc719260734ca4185360fa79
tobikoleriari <peter.leriari@strathmore.edu>
2024-09-11 15:32:24 +0300

    heading

--- Content/headings.php
+++ Content/headings.php
@@ -3,9 +3,9 @@ class headings{
     public function main_banner(){
         ?>
         <div class="banner">
-            <h1>Heading</h1>
+            <h1>HEADING</h1>
         </div>
             <?php
-            
+
     }
 }
