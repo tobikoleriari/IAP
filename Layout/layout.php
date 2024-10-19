0617490a42fdedd44158a04715065551611eaaed
tobikoleriari <peter.leriari@strathmore.edu>
2024-10-19 11:51:56 +0300

    ''

--- Layout/layout.php
+++ Layout/layout.php
@@ -10,6 +10,7 @@ class layout{
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link rel = "stylesheet" href="css\style.css"/>
             <title>Document</title>
+            
         </head>
         <body>
             <?php
